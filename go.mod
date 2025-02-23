48b80116f7e095c95326f8e642f4a65eb04fb2c3
aboguz <realyiudb5@gmail.com>
2025-02-23 14:13:18 -0500

    start rewriting. add prepare sql for creating

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/arthurshafikov/cryptobot-sdk-golang v1.1.6
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
 	modernc.org/sqlite v1.33.1
 )
 
--- sql.go
+++ sql.go
@@ -3,34 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+
 )
-
-type Storage struct {
-	db *sql.DB
+const (
+	path string = "gogi"
+)
+func OpenDB(){
+	db, err := sql.Open("sqlite3", path) 
 }
 
-func New(path string) (*Storage, error){
-	db, err := sql.Open(driverName:"sqlite3", path)
-	if err != nil {
-		return nil, fmt.Erorrf("Can't open DataBase: %w", err)
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, fmt.Erorrf("Can't CONNECT to DataBase: %w", err)
-	} 
-	return *Storage{db: db}, nil
-}
-
-func Save(s *Storage) Save(p *storage.Page) error {
-	q := `INSERT INTO pages (name, cid) VALUES(?,?)`
-	if _, err := s.db.ExecContext(ctx, q, p.Name, p,CID); err != nil{
-		return fmt.Errorf("Can't save page: %w", err)
-	}
-}
-
-func (s *Storage) Init(ctx context.Context) error {
-	q := `CREATE TABLE IF NOT EXIST pages (name TEXT, chatId INTEGER)`
-	if _, err := s.db.ExecContext(ctx, q); err != nil{
-		return fmt.Errorf("Can't create table: %w", err)
-	}
+func CreateDB(){
+	db, err := sql.Open("sqlite3", path)
+	query := db.Prepare("CREATE TABLE IF NOT EXIST users (chat_id INTEGER PRIMARY KEY, linkname TEXT, username TEXT, balance DECIMAL(15,2), registration_time INTEGER)")
 }
